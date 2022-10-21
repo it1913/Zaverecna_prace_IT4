9fc7bfd7206056120b85e89d3e7808d97fc3ea5a
it1913 <66723368+it1913@users.noreply.github.com>
2022-10-21 13:44:03 +0200

    2.3 Pročištění kódu

--- slave-wemos/src/main.cpp
+++ slave-wemos/src/main.cpp
@@ -2,45 +2,24 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 
-uint8_t broadcastAddress[] = {0xAC, 0x0B, 0xFB, 0xDC, 0x91, 0x79};
 const char LED = D5;
 const char button = D1;
-// Structure example to receive data
-// Must match the sender structure
+int LEDSTATE = 0;
+uint8_t broadcastAddress[] = {0xAC, 0x0B, 0xFB, 0xDC, 0x91, 0x79};
+
 typedef struct struct_message {
-    char a[32];
     int b;
-    float c;
-    String d;
-    bool e;
 } struct_message;
 
 typedef struct struct_answer{
   int ledS;
 }struct_answer;
 
-int LEDSTATE = 0;
-// Create a struct_message called myData
 struct_message myData;
 struct_answer ledData;
-String success;
 
-// callback function that will be executed when data is received
 void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
   memcpy(&myData, incomingData, sizeof(myData));
-  Serial.print("Bytes received: ");
-  Serial.println(len);
-  Serial.print("Char: ");
-  Serial.println(myData.a);
-  Serial.print("Int: ");
-  Serial.println(myData.b);
-  Serial.print("Float: ");
-  Serial.println(myData.c);
-  Serial.print("String: ");
-  Serial.println(myData.d);
-  Serial.print("Bool: ");
-  Serial.println(myData.e);
-  Serial.println();
   LEDSTATE = myData.b;
   digitalWrite(LED, HIGH);
 }
@@ -49,31 +28,27 @@ void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
   Serial.print("Last Packet Send Status: ");
   if (sendStatus == 0){
     Serial.println("Delivery success");
-  }else {
-  success = "Delivery Fail :(";
+  }
+  else{
+    Serial.println("Delivery fail");
   }
 }
 
 void setup() {
-  // Initialize Serial Monitor
   pinMode(button,INPUT_PULLUP);
   pinMode(LED, OUTPUT);
   Serial.begin(115200);
-  
-  // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
-  // Init ESP-NOW
   if (esp_now_init() != 0) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
   
-  // Once ESPNow is successfully Init, we will register for recv CB to
-  // get recv packer info
   esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
   esp_now_register_recv_cb(OnDataRecv);
   esp_now_register_send_cb(OnDataSent);
+  
   esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_COMBO, 1, NULL, 0);
 }
 
--- wemos-master/src/main.cpp
+++ wemos-master/src/main.cpp
@@ -4,29 +4,20 @@
 
 const char button = D2;
 int LEDSTATE = 0;
-// REPLACE WITH RECEIVER MAC Address
-
 uint8_t broadcastAddress[] = {0x68, 0xC6, 0x3A, 0xA4, 0xC9, 0x60};
 
-// Structure example to send data
-// Must match the receiver structure
 typedef struct struct_message {
-  char a[32];
   int b;
-  float c;
-  String d;
-  bool e;
 } struct_message;
 
 typedef struct struct_answer{
   int ledS;
 }struct_answer;
 
-// Create a struct_message called myData
 struct_message myData;
 struct_answer ledData;
 
-// Callback when data is sent
+
 void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
   Serial.print("Last Packet Send Status: ");
   if (sendStatus == 0){
@@ -36,8 +27,8 @@ void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
     Serial.println("Delivery fail");
   }
 }
- 
- void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
+
+void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
   memcpy(&ledData, incomingData, sizeof(ledData));
   Serial.print("Ledka je: ");
   Serial.println(ledData.ledS);
@@ -45,41 +36,27 @@ void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
 }
 
 void setup() {
-  pinMode(button,INPUT_PULLUP); //button
-  // Init Serial Monitor
+  pinMode(button,INPUT_PULLUP);
   Serial.begin(115200);
- 
-  // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
-  // Init ESP-NOW
   if (esp_now_init() != 0) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
 
-  // Once ESPNow is successfully Init, we will register for Send CB to
-  // get the status of Trasnmitted packet
   esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
   esp_now_register_recv_cb(OnDataRecv);
   esp_now_register_send_cb(OnDataSent);
-  
-  // Register peer
+
   esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
 }
 
 void loop() {
   if(digitalRead(button)==HIGH && LEDSTATE ==0){
-    // Set values to send
-    strcpy(myData.a, "THIS IS A CHAR");
     myData.b = 1;
-    myData.c = 1.2;
-    myData.d = "Hello";
-    myData.e = false;
-    LEDSTATE = 1;
-    // Send message via ESP-NOW
     esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
     delay(500);
   }
- 
+
 }
