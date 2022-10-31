ee4e26184e67ac2017bb26f0bee9c5d46787bf95
Martin Kavala <66723368+it1913@users.noreply.github.com>
2022-10-31 19:01:49 +0100

    2.5 Pomocné programy

--- pomocne_programy/Web/src/main.cpp
+++ pomocne_programy/Web/src/main.cpp
@@ -7,21 +7,14 @@ const char* ssid = "Lego3"; //Write your SSID
 const char* password = "AdelaMartinPetraIvo";   //Write your password
 
 const char* input_parameter = "state";
-const char* input_parameter2 = "state2";
 
-const int output = 14;
-const int Push_button_GPIO = 4 ;
-const int output2 = 0; //2. led a button
-const int Push_button_GPIO2 = 5;
+const int output = 2;
+const int Push_button_GPIO = 5;
 
 // Variables will change:
 int LED_state = LOW;         
 int button_state;             
-int lastbutton_state = LOW;
-// Once more
-int LED_state2 = LOW;         
-int button_state2;             
-int lastbutton_state2 = LOW;      
+int lastbutton_state = LOW;   
 
 unsigned long lastDebounceTime = 0;  
 unsigned long debounceDelay = 50;    
@@ -50,17 +43,10 @@ const char index_html[] PROGMEM = R"rawliteral(
 <body>
   <h2>ESP Output Control Web Server</h2>
   %BUTTONPLACEHOLDER%
-  %BUTTONPLACEHOLDER2%
 <script>function toggleCheckbox(element) {
   var xhr = new XMLHttpRequest();
   if(element.checked){ xhr.open("GET", "/update?state=1", true); }
   else { xhr.open("GET", "/update?state=0", true); }
-  xhr.send();
-}
-function toggleCheckbox2(element) {
-  var xhr = new XMLHttpRequest();
-  if(element.checked){ xhr.open("GET", "/update?state2=1", true); }
-  else { xhr.open("GET", "/update?state2=0", true); }
   xhr.send();
 }
 
@@ -84,25 +70,6 @@ setInterval(function ( ) {
   };
   xhttp.open("GET", "/state", true);
   xhttp.send();
-
-  var xhttp2 = new XMLHttpRequest();
-  xhttp2.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var inputChecked2;
-      var outputStateM2;
-      if( this.responseText == 1){ 
-        inputChecked2 = true;
-        outputStateM2 = "ON";
-      }
-      else { 
-        inputChecked2 = false;
-        outputStateM2 = "OFF";
-      }
-      document.getElementById("output2").checked = inputChecked;
-      document.getElementById("outputState2").innerHTML = outputStateM;
-    }
-  xhttp2.open("GET", "/state2", true);
-  xhttp2.send();
 }, 1000 ) ;
 </script>
 </body>
@@ -119,34 +86,17 @@ String outputState(){
   return "";
 }
 
-String outputState2()
-{
-if(digitalRead(output2))
-{
-return "checked";
-}
-else
-{
-return "";
-}
-return "";
-}
-
 String processor(const String& var){
   if(var == "BUTTONPLACEHOLDER"){
-    String buttons1 ="";
+    String buttons ="";
     String outputStateValue = outputState();
-    buttons1+= "<h4>Output State: <span id=\"outputState\"></span></h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"output\" " + outputStateValue + "><span class=\"slider\"></span></label>";
-    return buttons1;
-  }
-  if(var == "BUTTONPLACEHOLDER2"){
-    String buttons2 ="";
-    String outputStateValue2 = outputState2();
-    buttons2+= "<h4>Output State: <span id=\"outputState2\"></span></h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox2(this)\" id=\"output2\" " + outputStateValue2 + "><span class=\"slider\"></span></label>";
-    return buttons2;
+    buttons+= "<h4>Output State: <span id=\"outputState\"></span></h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"output\" " + outputStateValue + "><span class=\"slider\"></span></label>";
+    return buttons;
   }
   return String();
 }
+
+
 
 void setup(){
   // Serial port for debugging purposes
@@ -155,10 +105,6 @@ void setup(){
   pinMode(output, OUTPUT);
   digitalWrite(output, LOW);
   pinMode(Push_button_GPIO, INPUT);
-
-  pinMode(output2, OUTPUT);
-  digitalWrite(output2, LOW);
-  pinMode(Push_button_GPIO2, INPUT);
   
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
@@ -180,9 +126,6 @@ void setup(){
   server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
     String input_message;
     String inputParameter;
-    String input_message2;
-    String inputParameter2;
-
     // GET input1 value on <ESP_IP>/update?state=<input_message>
     if (request->hasParam(input_parameter)) {
       input_message = request->getParam(input_parameter)->value();
@@ -196,28 +139,11 @@ void setup(){
     }
     Serial.println(input_message);
     request->send(200, "text/plain", "OK");
-
-    if (request->hasParam(input_parameter2)) {
-      input_message2 = request->getParam(input_parameter2)->value();
-      inputParameter2 = input_parameter2;
-      digitalWrite(output2, input_message2.toInt());
-      LED_state2 = !LED_state2;
-    }
-    else {
-      input_message2 = "No message sent";
-      inputParameter2 = "none";
-    }
-    Serial.println(input_message2);
-    request->send(200, "text/plain", "OK");
   });
 
   
   server.on("/state", HTTP_GET, [] (AsyncWebServerRequest *request) {
     request->send(200, "text/plain", String(digitalRead(output)).c_str());
-  });
-
-   server.on("/state", HTTP_GET, [] (AsyncWebServerRequest *request) {
-    request->send(200, "text/plain", String(digitalRead(output2)).c_str());
   });
   // Start server
   server.begin();
@@ -225,7 +151,6 @@ void setup(){
   
 void loop() {
   int data = digitalRead(Push_button_GPIO);
-  int data2 = digitalRead(Push_button_GPIO2);
 
   if (data != lastbutton_state) {
     // reset the debouncing timer
@@ -241,21 +166,11 @@ void loop() {
         LED_state = !LED_state;
       }
     }
-    if (data2 != button_state2) {
-      button_state2 = data2;
-
-
-      if (button_state2 == HIGH) {
-        LED_state2 = !LED_state2;
-      }
-    }
   }
 
   
   digitalWrite(output, LED_state);
-  digitalWrite(output2, LED_state2);
 
 
   lastbutton_state = data;
-  lastbutton_state2 = data2;
 }
